1125efce7cf32f1e88e0a165eda9d9fb8b775d51
zolezzi <cezcardozo23@gmail.com>
2018-05-06 01:27:31 -0300

    add implementations services

---
+++ src/main/java/edu/unq/desapp/grupo_a/backend/service/PublicationServiceImpl.java
@@ -0,0 +1,22 @@
+package edu.unq.desapp.grupo_a.backend.service;
+
+import java.util.List;
+
+import edu.unq.desapp.grupo_a.backend.model.Publication;
+import edu.unq.desapp.grupo_a.backend.model.User;
+
+public class PublicationServiceImpl implements PublicationService{
+
+	@Override
+	public List<Publication> getPublicationVehicles(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Publication> searchPublication(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/edu/unq/desapp/grupo_a/backend/service/VehicleServiceImpl.java
@@ -0,0 +1,61 @@
+package edu.unq.desapp.grupo_a.backend.service;
+
+import java.util.List;
+
+import edu.unq.desapp.grupo_a.backend.dto.VehicleDto;
+import edu.unq.desapp.grupo_a.backend.model.User;
+import edu.unq.desapp.grupo_a.backend.model.Vehicle;
+import edu.unq.desapp.grupo_a.backend.model.VehicleData;
+import edu.unq.desapp.grupo_a.backend.model.VehicleFilter;
+
+public class VehicleServiceImpl implements VehicleService{
+
+	@Override
+	public void addVehicleData(VehicleData vehicleData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addVehicle(VehicleDto vehicleDto, User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public VehicleDto getVehicle(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateVehicle(Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteVehicle(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void publishVehicle(Vehicle vehicle, User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rentVehicle(Vehicle vehicle, User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Vehicle> searchVehicle(VehicleFilter vehicleFilter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
