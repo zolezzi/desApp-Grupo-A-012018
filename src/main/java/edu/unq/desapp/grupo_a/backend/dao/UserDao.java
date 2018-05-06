74de937ab323d7b8c931155981d02cadb0559266
zolezzi <cezcardozo23@gmail.com>
2018-05-06 01:31:06 -0300

    add class example UserDao, (refactor name a repository remem)

---
+++ src/main/java/edu/unq/desapp/grupo_a/backend/dao/UserDao.java
@@ -0,0 +1,23 @@
+package edu.unq.desapp.grupo_a.backend.dao;
+
+import org.springframework.stereotype.Repository;
+
+import edu.unq.desapp.grupo_a.backend.model.User;
+
+@Repository
+public class UserDao extends HibernateGenericDao<User> implements GenericDao<User> {
+
+	private static final long serialVersionUID = -4425722631916607857L;
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public UserDao() {	}
+
+	@Override
+	public Class<User> getDomainClass() {
+		return User.class;
+	}
+	
+}
