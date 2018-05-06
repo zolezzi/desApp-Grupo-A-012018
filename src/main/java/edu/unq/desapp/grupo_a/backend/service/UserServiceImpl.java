e542cbc420b5dade3d5987114019b820a30e3490
zolezzi <cezcardozo23@gmail.com>
2018-05-06 01:27:31 -0300

    add implementations services

---
+++ src/main/java/edu/unq/desapp/grupo_a/backend/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package edu.unq.desapp.grupo_a.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.unq.desapp.grupo_a.backend.dao.UserDao;
+import edu.unq.desapp.grupo_a.backend.dto.UserDto;
+import edu.unq.desapp.grupo_a.backend.model.User;
+
+@Transactional
+@Service
+public class UserServiceImpl extends GenericService<User> {
+
+	@Autowired
+	private UserDao repository;
+
+	public UserServiceImpl() {
+	}
+
+	public UserServiceImpl(UserDao repo) {
+		this.setRepository(repo);
+	}
+
+	public UserDao getRepository() {
+		return repository;
+	}
+
+	public void setRepository(UserDao repository) {
+		this.repository = repository;
+	}
+
+	public void saveuser(UserDto dto) {
+	
+		
+	}
+
+}
