96fac337eef50971d28530071bdf8e3032151349
zolezzi <cezcardozo23@gmail.com>
2018-06-05 18:33:27 -0300

    Add resource and implementation the users and services

--- src/main/java/edu/unq/desapp/grupo_a/backend/service/UserServiceImpl.java
+++ src/main/java/edu/unq/desapp/grupo_a/backend/service/UserServiceImpl.java
@@ -1,36 +1,74 @@
 package edu.unq.desapp.grupo_a.backend.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import edu.unq.desapp.grupo_a.backend.service.UserService;
+import edu.unq.desapp.grupo_a.backend.validators.UserValidator;
 import edu.unq.desapp.grupo_a.backend.dao.UserRepository;
 import edu.unq.desapp.grupo_a.backend.model.User;
+import edu.unq.desapp.grupo_a.backend.model.exceptions.UserInitException;
 
-@Transactional
+
 @Service
-public class UserServiceImpl extends GenericService<User> {
+public class UserServiceImpl implements UserService {
 
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
 
+	@Autowired
+	private UserValidator userValidator;
+	
 	public UserServiceImpl() {
 	}
 
-	public UserServiceImpl(UserRepository repo) {
-		this.setRepository(repo);
-	}
-
-	public UserRepository getRepository() {
-		return repository;
-	}
-
-	public void setRepository(UserRepository repository) {
-		this.repository = repository;
-	}
-
-	//ToDo... implement
-	//public void saveuser(UserDto dto) {
+	@Override
+	public void createUser(User user) {
 		
-	//}
+		try {
+			
+			userValidator.ValidateUser(user);
+			
+			userRepository.save(user);
+
+		}catch(UserInitException e){
+			
+		}
+
+		
+	}
+
+	@Override
+	public User getUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User update(User user) {
+		
+		return null;
+		
+	}
+
+	@Override
+	public void offerVehicle(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> searchUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/main/java/edu/unq/desapp/grupo_a/backend/webservice/UserResourceImpl.java
+++ src/main/java/edu/unq/desapp/grupo_a/backend/webservice/UserResourceImpl.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,40 +14,44 @@ import edu.unq.desapp.grupo_a.backend.api.UserResource;
 import edu.unq.desapp.grupo_a.backend.dao.UserRepository;
 import edu.unq.desapp.grupo_a.backend.dto.UserDto;
 import edu.unq.desapp.grupo_a.backend.model.User;
-import edu.unq.desapp.grupo_a.backend.service.GenericService;
 import edu.unq.desapp.grupo_a.backend.service.UserServiceImpl;
 
 @Service
 @Produces("application/json")
 @Consumes("application/json")
 @Path("/users")
-public class UserResourceImpl extends GenericRest<User>  implements UserResource{
+public class UserResourceImpl implements UserResource{
 	
 	@Autowired
 	private UserServiceImpl userService;
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	ModelMapper modelMapper; 
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
-		
-		try {
-			
-			//Mapper
 
-		}catch(Exception e) {
 			
-		}
+		User user =	modelMapper.map(userDto, User.class);
 		
-		
-		return null;
+		userService.createUser(user);		
+	
+		return userDto;
+
 	}
 
 	@Override
-	public UserDto update(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDto update(UserDto userDto) {
+			
+		User user =	modelMapper.map(userDto, User.class);
+		
+		User userUpdate = userService.update(user);
+		
+		return modelMapper.map(userUpdate, UserDto.class);
+	
 	}
 
 	@Override
@@ -57,12 +62,6 @@ public class UserResourceImpl extends GenericRest<User>  implements UserResource
 
 	@Override
 	public List<UserDto> searchUsers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public GenericService<User> getService() {
 		// TODO Auto-generated method stub
 		return null;
 	}
